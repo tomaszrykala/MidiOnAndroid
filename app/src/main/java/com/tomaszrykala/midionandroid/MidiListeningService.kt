73ca221f80e730351aa86bf9442e85bece3b2992
Tomasz Rykała <tomasz.a.rykala@gmail.com>
2018-04-02 13:10:33 +0100

    Add MidiListeningService (#2)

---
+++ app/src/main/java/com/tomaszrykala/midionandroid/MidiListeningService.kt
@@ -0,0 +1,64 @@
+package com.tomaszrykala.midionandroid
+
+import android.media.midi.MidiDeviceService
+import android.media.midi.MidiDeviceStatus
+import android.media.midi.MidiReceiver
+import android.util.Log
+import com.tomaszrykala.midionandroid.midi.MidiEvent
+import com.tomaszrykala.midionandroid.midi.MidiEventType
+
+class MidiListeningService : MidiDeviceService() {
+
+    companion object {
+        const val TAG: String = "MidiListeningService"
+    }
+
+    private var isRunning: Boolean = false
+
+    override fun onGetInputPortReceivers(): Array<MidiReceiver> = arrayOf(midiReceiver)
+
+    private val midiEventTypes: MutableMap<Byte, MidiEventType> by lazy {
+        mutableMapOf<Byte, MidiEventType>().apply {
+            MidiEventType.values().asIterable().map {
+                this.put(it.byte, it)
+            }
+        }
+    }
+
+    private val midiReceiver = object : MidiReceiver() {
+
+        fun start() {
+            Log.d(TAG, "start")
+        }
+
+        fun stop() {
+            Log.d(TAG, "stop")
+        }
+
+        override fun onSend(msg: ByteArray?, offset: Int, count: Int, timestamp: Long) {
+            msg?.run {
+                midiEventTypes[(msg[1] - msg[4]).toByte()]?.run {
+                    Log.d(TAG, MidiEvent(this, msg[4], msg[2], msg[3]).toString())
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        midiReceiver.stop()
+    }
+
+    override fun onDeviceStatusChanged(status: MidiDeviceStatus?) {
+        super.onDeviceStatusChanged(status)
+        status?.run {
+            if (status.isInputPortOpen(0) && !isRunning) {
+                midiReceiver.start()
+                isRunning = true
+            } else if (!status.isInputPortOpen(0) && isRunning) {
+                midiReceiver.stop()
+                isRunning = false
+            }
+        }
+    }
+}
